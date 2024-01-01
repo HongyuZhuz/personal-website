6c5f61c61aef7efcefacef4c266bdae288021506
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-01-01 21:27:22 +1100

    changed the title of page

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
 import { Header } from './components/Header'
+import {Metadata} from 'next'
+
+export const metadata:Metadata = {
+  title:'Hongyu Zhu'
+}
 
 export default function Home() {
   return (
-
+    <>
     <div>
       <Header />
     </div>
+    
+    </>
+    
   )
 }
