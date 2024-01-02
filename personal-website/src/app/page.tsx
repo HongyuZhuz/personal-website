a7bd49856b7b1fbe15ab40c24d7fa76986754496
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-01-03 10:34:56 +1100

    test the AWS S3

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { HeroSection } from './components/heroSection'
 import { AboutMe } from './components/about'
 import { MyEducationSection } from './components/education'
@@ -6,11 +5,66 @@ import { Designer } from './components/designer'
 import { Footer } from './components/footer'
 
 
+import crypto from "crypto";
+import { Bucket } from "sst/node/bucket";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { GetServerSideProps } from 'next';
 
-export default function Home() {
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // 这里可以获取数据，例如从 API 或其他服务
+  const command = new PutObjectCommand({
+    ACL: "public-read",
+    Key: crypto.randomUUID(),
+    Bucket: Bucket.public.bucketName,
+  });
+  const url = await getSignedUrl(new S3Client({}), command);
+
+  return { props: { url } };
+
+  // 返回的 props 将会传递给页面组件
+};
+/*export async function getServerSideProps() {
+  const command = new PutObjectCommand({
+    ACL: "public-read",
+    Key: crypto.randomUUID(),
+    Bucket: Bucket.public.bucketName,
+  });
+  const url = await getSignedUrl(new S3Client({}), command);
+
+  return { props: { url } };
+}*/
+
+export default function Home({url}:{url:string}) {
   return (
     <>
     <div>
+    <main>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          const file = (e.target as HTMLFormElement).file.files?.[0]!;
+
+          const image = await fetch(url, {
+            body: file,
+            method: "PUT",
+            headers: {
+              "Content-Type": file.type,
+              "Content-Disposition": `attachment; filename="${file.name}"`,
+            },
+          });
+
+          window.location.href = image.url.split("?")[0];
+        }}
+      >
+        <input name="file" type="file" accept="image/png, image/jpeg" />
+        <button type="submit">
+          Upload
+        </button>
+      </form>
+    </main>
       <HeroSection />
       <AboutMe />
       <MyEducationSection />
@@ -21,3 +75,5 @@ export default function Home() {
     </>
   )
 }
+
+
