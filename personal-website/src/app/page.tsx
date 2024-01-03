23e6641d19919e419837ef37601e70c37873c391
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-01-03 13:11:19 +1100

    now I can access the AWS E3

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,16 +1,40 @@
-import Image from 'next/image'
+'use client'
 import { HeroSection } from './components/heroSection'
 import { AboutMe } from './components/about'
 import { MyEducationSection } from './components/education'
 import { Designer } from './components/designer'
 import { Footer } from './components/footer'
+import { useState } from 'react'
+import aws from '../../creatingAws'
 
 
 
 export default function Home() {
+  const [message, setMessage] = useState();
+  const [file,setFile] = useState();
+
+  function storeFile(e:any){
+    console.log("test3");
+    setFile(e.target.files[0]);
+  }
+
+  const uploadFile = async()=>{
+    setMessage("uploading!");
+    var returnData = await aws(file);//send file to api endpoint
+
+    setMessage(String(returnData));
+    setFile(null);
+  }
+
   return (
     <>
     <div>
+      <div className='flex bg-white w-full text-black p-20'>
+        <p>Upload file:</p>
+        <p className='text-red-500'>{message}</p>
+        <input type="file" onChange = {(e)=>storeFile(e)}/>
+        <input type = "button" onClick = {uploadFile} defaultValue = "Send"/>
+      </div>
       <HeroSection />
       <AboutMe />
       <MyEducationSection />
@@ -21,3 +45,4 @@ export default function Home() {
     </>
   )
 }
+
