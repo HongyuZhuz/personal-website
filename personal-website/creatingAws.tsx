a0d5c6506a8828eebc29b27342a36333f807fa59
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-01-03 13:11:19 +1100

    now I can access the AWS E3

---
+++ personal-website/creatingAws.tsx
@@ -0,0 +1,37 @@
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { PutObjectCommand, S3 } from "@aws-sdk/client-s3";
+import axios from "axios";
+
+const s3 = new S3({
+    region:"ap-southeast-2",
+
+    credentials: {
+        accessKeyId:"AKIAVGNQRL46SARFROIN",
+        secretAccessKey:"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",
+    },
+
+})
+
+export default async function aws(file:File){
+    try{
+        
+        const fileParams = {
+            Bucket:"hyzawsbucket",
+            Key:file.name,
+        }
+        const url = await getSignedUrl(s3, new PutObjectCommand(fileParams),{expiresIn:3600});
+        console.log(url)
+        await axios.put(url, file, {
+            headers:
+            {
+                "Content-type":String(file.type)
+            }
+        })
+
+        return "Uploaded!";
+    }
+    catch(err){
+        return err;
+    }
+
+}
