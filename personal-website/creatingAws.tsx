ae6d95b87a00daa62ee433c9ba63434f297c78c2
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-01-03 20:30:34 +1100

    ...

--- personal-website/creatingAws.tsx
+++ personal-website/creatingAws.tsx
@@ -1,15 +1,16 @@
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { PutObjectCommand, S3 } from "@aws-sdk/client-s3";
 import axios from "axios";
+import { kv } from '@vercel/kv';
+import { NextResponse } from 'next/server';
+
 
 const s3 = new S3({
     region:"ap-southeast-2",
-
     credentials: {
-        accessKeyId:"a",
-        secretAccessKey:"b",
+        accessKeyId:process.env.ACCESS_KEY_ID,
+        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
     },
-
 })
 
 export default async function aws(file:File){
@@ -19,6 +20,9 @@ export default async function aws(file:File){
             Bucket:"hyzawsbucket",
             Key:file.name,
         }
+        console.log(process.env.KV_REST_API_READ_ONLY_TOKEN);
+        console.log(process.env.ACCESS_KEY_ID)
+
         const url = await getSignedUrl(s3, new PutObjectCommand(fileParams),{expiresIn:3600});
         console.log(url)
         await axios.put(url, file, {
